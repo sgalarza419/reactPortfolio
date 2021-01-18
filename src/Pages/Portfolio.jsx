eb39e37910fa68d04b2e6dc6408bb7e8b6e65170
S Galarza <sgalarza4190@gmail.com>
2021-01-18 01:19:28 -0500

    fixed portfolio page

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,5 +1,12 @@
 import {useState} from "react";
 import {Card} from "react-bootstrap";
+import horiseon from "../assets/img/Horiseon.PNG";
+import randomPassword from "../assets/img/Random Password.PNG";
+import javaQuiz from "../assets/img/Java Quiz.PNG";
+import workDay from "../assets/img/Work Day Planner.PNG";
+import weatherDash from "../assets/img/Weather Dashboard.PNG";
+import spiceItUp from "../assets/img/SpiceItUp.PNG";
+
 const Portfolio = () => {
     const [projects, setProjects] = useState([
         {
@@ -7,7 +14,7 @@ const Portfolio = () => {
             body: `This project helped me to understand the importance of code syntax and organization. I consolidated the
             CSS file and added classes to the HTML file to make it clearer for user to understand and easier
             to read.`,
-            image: '../assets/img/Horiseon.PNG',
+            image: horiseon,
             link: 'https://sgalarza419.github.io/codeRefractor/',
             id: 1
         },
@@ -17,7 +24,7 @@ const Portfolio = () => {
             project taught me the importance
             of else if statements when selecting between multiple user input. I also learned how to change
             and impliment arrays using multiple methods.`,
-            image: '../assets/img/Random Password.PNG',
+            image: randomPassword,
             link: 'https://sgalarza419.github.io/randomPassword/',
             id: 2
         },
@@ -25,7 +32,7 @@ const Portfolio = () => {
             title: 'Video Game Quiz',
             body: `Test your knowledge on the video game industry in this challenging quiz. I
             learned how to impliment an incremental timer and the importance of local storage.`,
-            image: '../assets/img/Java Quiz.PNG',
+            image: javaQuiz,
             link: 'https://sgalarza419.github.io/JavaScriptQuiz/',
             id: 3
         },
@@ -35,7 +42,7 @@ const Portfolio = () => {
             based on the hour of work. I learned how to use moment to keep track of the current time. I used
             moment to change the color of a note background based on the time and local storage to keep
             daily inputs when the page is refreshed.`,
-            image: '../assets/img/Work Day Planner.PNG',
+            image: workDay,
             link: 'https://sgalarza419.github.io/workCalendar/',
             id: 4
         },
@@ -44,7 +51,7 @@ const Portfolio = () => {
             body: `Need to find today or the five day forecast? Use this website to get the
             weather of cities from around the world. The use of multiple APIs and local storage the website
             comes alive! Try it out and check the weather in your area today.`,
-            image: '../assets/img/Weather Dashboard.PNG',
+            image: weatherDash,
             link: 'https://sgalarza419.github.io/weatherPanel/',
             id: 5
         },
@@ -53,26 +60,28 @@ const Portfolio = () => {
             body: `In this app you will combat boredom by selecting an activity to randomly be
             generated. You can save your newly inspired venture for another day if the weather doesn't
             permit.`,
-            image: '../assets/img/Weather Spice It Up!.PNG',
+            image: spiceItUp,
             link: 'https://dspark8916.github.io/Project1Group2/',
             id: 6
         }
     ])
 
     return ( 
-<div className="card">
+<div className="row row-cols-1 row-cols-md-2">
+<div className="card col mb-4">
 {projects.map((project) => (
-<Card style={{ width: '18rem' }} key={projects.id}>
-  <Card.Img variant="top" src={projects.image} />
+<Card style={{ width: '18rem' }} key={project.id}>
+  <Card.Img variant="top" src={project.image} />
   <Card.Body>
-    <Card.Title>{projects.title}</Card.Title>
+    <Card.Title>{project.title}</Card.Title>
     <Card.Text>
-    {projects.body}
+    {project.body}
     </Card.Text>
-    <Card.Link href={projects.link}>{projects.title}</Card.Link>
+    <Card.Link href={project.link}>{project.title}</Card.Link>
   </Card.Body>
 </Card>
 ))}
+</div>
 </div>
     )
 }
